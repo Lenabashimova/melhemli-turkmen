ac35e88ffbe54aaf00fdf2959283ad91d382f309
unknown <lena5imova@gmail.com>
2026-09-10 11:14:25 +0500

    updated header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -44,16 +44,16 @@ export default function Header(){
                 <div className="sm:mx-auto flex h-20 items-center justify-between px-6">
                     {/* Mobile */}
                     <div className="flex w-full items-center justify-between lg:hidden">
-                        <Image src="/logo-desktop.png" width={119} height={34} alt="logo"/>
-                        <div className="flex items-center gap-2 text-[#B42226]">
+                        <Image src="/logo-new.png" width={119} height={34} alt="logo"/>
+                        <div className="flex items-center gap-2 text-[#1FAC4D]">
                             {languages.map((lang) => (
                                 <button
                                     key={lang}
                                     onClick={() => changeLanguage(lang)}
-                                    className={`border border-[#B42226] rounded-sm p-2 cursor-pointer transition-colors ${
+                                    className={`border border-[#1FAC4D] rounded-sm p-2 cursor-pointer transition-colors ${
                                         currentLanguage === lang
-                                            ? "bg-[#B42226] text-white"
-                                            : "hover:bg-[#B42226] hover:text-white"
+                                            ? "bg-[#1FAC4D] text-white"
+                                            : "hover:bg-[#1FAC4D] hover:text-white"
                                     }`}
                                 >
                                     {lang.toUpperCase()}
@@ -62,18 +62,18 @@ export default function Header(){
                         </div>
                     </div>
                     {/* Desktop */}
-                    <div className="hidden w-full items-center justify-between lg:flex bg-white text-[#275283de] lg:border-[#0C233E] lg:border-3 rounded-2xl px-11 py-5 mt-15 mx-10">
-                        <Image src="/logo-desktop.png" width={166} height={48} alt="logo" className="cursor-pointer"/>
+                    <div className="hidden w-full items-center justify-between lg:flex bg-white text-[#222D65] lg:border-[#1FAC4D] lg:border-3 rounded-2xl px-11 py-5 mt-15 mx-10">
+                        <Image src="/logo-new.png" width={200} height={40} alt="logo" className="cursor-pointer"/>
                         <nav className="flex items-center gap-19">
-                            <Link href={'#home'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#0C233E] text-[24px]">{t.header.home}</Link>
-                            <Link href={'#programs'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#0C233E] text-[24px]">{t.header.services}</Link>
-                            <Link href={'#process'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#0C233E] text-[24px]">{t.header.projects}</Link>
-                            <Link href={'#news'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#0C233E] text-[24px]">{t.header.news}</Link>
-                            <Link href={'#footer'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#0C233E] text-[24px]">{t.header.contacts}</Link>
+                            <Link href={'#home'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#1FAC4D] text-[24px]">{t.header.home}</Link>
+                            <Link href={'#programs'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#1FAC4D] text-[24px]">{t.header.services}</Link>
+                            <Link href={'#process'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#1FAC4D] text-[24px]">{t.header.projects}</Link>
+                            <Link href={'#news'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#1FAC4D] text-[24px]">{t.header.news}</Link>
+                            <Link href={'#footer'} className="cursor-pointer hover:[text-shadow:0_0_0_currentColor] hover:text-[#1FAC4D] text-[24px]">{t.header.contacts}</Link>
                         </nav>
-                        <div className="flex items-center gap-2 text-[#B42226]">
+                        <div className="flex items-center gap-2 text-[#1FAC4D]">
                             {languages.map((lang) => (
-                                <button key={lang} onClick={() => changeLanguage(lang)} className={`border-[#B42226] border rounded-sm p-4 cursor-pointer ${language === lang? "bg-[#B42226] text-white" : "hover:text-white hover:bg-[#B42226]"}`}>
+                                <button key={lang} onClick={() => changeLanguage(lang)} className={`border-[#1FAC4D] border rounded-sm p-4 cursor-pointer ${language === lang? "bg-[#1FAC4D] text-white" : "hover:text-white hover:bg-[#1FAC4D]"}`}>
                                     {lang.toUpperCase()}
                                 </button>
                             ))}
